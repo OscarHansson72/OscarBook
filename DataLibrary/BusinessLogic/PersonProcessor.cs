4181a0f61d3694be9215e13a584b18e45a22bf8f
Oscar Hansson_c <oscar.hansson_c@sandvik.com>
2024-06-18 15:43:19 +0200

    initial commit

---
+++ DataLibrary/BusinessLogic/PersonProcessor.cs
@@ -0,0 +1,32 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class PersonProcessor
+    {
+        public static int CreatePerson(string firstName, string lastName, string email,
+            string phoneNumber, DateTime dateOfBirth, string password)
+        {
+            PersonModel data = new PersonModel 
+            {   FirstName = firstName,
+                LastName = lastName, 
+                Email = email, 
+                PhoneNumber = phoneNumber, 
+                DateOfBirth = dateOfBirth, 
+                Password = password 
+            };
+
+            string sql = @"insert into dbo.Person (FirstName, LastName, Email, PhoneNumber, DataOfBirth, Password)
+                          values (@FirstName, @LastName, @Email, @PhoneNumber, @DateOfBirth, @Password";
+
+            return SQLDataAccess.SaveData(sql, data);
+        }
+    }
+}
