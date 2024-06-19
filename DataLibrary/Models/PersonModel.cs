e2b967f5d562621cc79ff9d270809feb42fc6193
Oscar Hansson_c <oscar.hansson_c@sandvik.com>
2024-06-19 14:39:51 +0200

    fixed dapper reference and accesibility to PersonModel

--- DataLibrary/Models/PersonModel.cs
+++ DataLibrary/Models/PersonModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataLibrary.Models
 {
-    internal class PersonModel
+    public class PersonModel
     {
         public int id { get; set; }
         public string FirstName { get; set; }
